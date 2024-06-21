87228d3370bc82c86ef916bf6066d46b924aa238
euginepro <87873293+euginepro@users.noreply.github.com>
2024-06-21 23:08:26 +0300

    Delete Hubman/saa_run6Salma.bat

--- Hubman/saa_run6Salma.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" cmd /c "runSalmac.bat"
-start "" cmd /c "runSalmaf.bat"
-start "" cmd /c "runSalmae.bat"
-start "" cmd /c "runSalmac.bat"
-start "" cmd /c "runSalmaf.bat"
-start "" cmd /c "runSalmae.bat"
