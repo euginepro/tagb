d1bbd484d012429f6c839a8763cc829b2a8375a9
euginepro <87873293+euginepro@users.noreply.github.com>
2024-06-21 23:06:47 +0300

    Delete Hubman/b_run3and3Salma.bat

--- Hubman/b_run3and3Salma.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" cmd /c "runSalmac.bat"
-start "" cmd /c "runSalmaf.bat"
-start "" cmd /c "runSalmae.bat"
-start "" cmd /c "runChrome.bat"
-start "" cmd /c "runEdge.bat"
-start "" cmd /c "runFirefox.bat"
