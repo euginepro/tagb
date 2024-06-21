788b7ca48b72761c34df81c991cd218c94b175ec
euginepro <87873293+euginepro@users.noreply.github.com>
2024-06-21 23:07:39 +0300

    Delete Hubman/b_run6Blairs.bat

--- Hubman/b_run6Blairs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" cmd /c "runChrome.bat"
-start "" cmd /c "runEdge.bat"
-start "" cmd /c "runFirefox.bat"
-start "" cmd /c "runChrome.bat"
-start "" cmd /c "runEdge.bat"
-start "" cmd /c "runFirefox.bat"
