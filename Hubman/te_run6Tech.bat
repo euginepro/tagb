2f1bf51b851de12fd3a22781e241e7e35b3112b1
euginepro <87873293+euginepro@users.noreply.github.com>
2024-06-21 23:07:23 +0300

    Delete Hubman/te_run6Tech.bat

--- Hubman/te_run6Tech.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" cmd /c "runTechChrome.bat"
-start "" cmd /c "runTechEdge.bat"
-start "" cmd /c "runTechFireFox.bat"
-start "" cmd /c "runTechChrome.bat"
-start "" cmd /c "runTechEdge.bat"
-start "" cmd /c "runTechFireFox.bat"
